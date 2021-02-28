127e6fe2f5ed76b470a184b5c98b2cff510c3a64
Rodrigo Candido <rodrigoipu@hotmail.com>
2021-02-28 18:17:10 +0100

    On branch createAppUI - Create internal folder structure Delete Storyboard and its configuration Add initial controller Create initial UI

--- LocalizedResume/LocalizedResume/Controller/ViewController.swift
+++ LocalizedResume/LocalizedResume/Controller/ViewController.swift
@@ -8,12 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var faceImage: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
 }
-
